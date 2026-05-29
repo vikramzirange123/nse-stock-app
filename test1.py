f1b4c311e829ae261524b8a6634562e0e3781a85
vikramzirange123 <vikramzirange123@gmail.com>
2026-05-29 12:52:37 -0400

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 import streamlit as st
-#import yfinance as yf
-import kiteconnect as yf
+import yfinance as yf
+#import kiteconnect as yf
 import pandas as pd
 import sys
 # -----------------------------
